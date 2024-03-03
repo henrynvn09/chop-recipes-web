b6e28e4da0d597b168098601c8905af7d65f4d28
henrynvn09 <henrynvn09@gmail.com>
2024-03-02 22:25:59 -0800

    Add routing to LibraryRecipes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './Pages/HomePage';
-import ContactPage from './Pages/ContactPage';
-import ErrorPage from './Pages/ErrorPage';
-import UserPage from './Pages/UserPage';
-import LoginPage from './Pages/LoginPage';
-import SignUpPage from './Pages/SignUpPage';
-import AboutPage from './Pages/AboutPage';  
-import UploadPage from './Pages/UploadPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import ContactPage from "./Pages/ContactPage";
+import ErrorPage from "./Pages/ErrorPage";
+import UserPage from "./Pages/UserPage";
+import LoginPage from "./Pages/LoginPage";
+import SignUpPage from "./Pages/SignUpPage";
+import AboutPage from "./Pages/AboutPage";
+import UploadPage from "./Pages/UploadPage";
 import Navbar from "./Components/Navbar";
-import ViewRecipe from './Pages/viewPage';
-import UploadRecipe from './Components/UploadRecipe';
-
-
-
+import ViewRecipe from "./Pages/viewPage";
+import UploadRecipe from "./Components/UploadRecipe";
+import LibraryRecipes from "./Pages/LibraryRecipes";
 
 function App() {
   return (
@@ -22,11 +20,12 @@ function App() {
           <Route path="/" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/home" element={<HomePage />} />
-          <Route path="/contacts" element={<ContactPage/>} />
-          <Route path="/about" element={<AboutPage />} /> 
-          <Route path="/upload-recipe" element={<UploadPage/>} />
-          <Route path="/view-recipe" element={<ViewRecipe/>} />
-          <Route path="/user" element={<UserPage/>} />
+          <Route path="/contacts" element={<ContactPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/upload-recipe" element={<UploadPage />} />
+          <Route path="/view-recipe" element={<ViewRecipe />} />
+          <Route path="/user" element={<UserPage />} />
+          <Route path="/library" element={<LibraryRecipes />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
