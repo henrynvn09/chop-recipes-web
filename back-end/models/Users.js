065faced2a3615a53350dace21bba0df07af2ead
Edmund <edmundyau1214@gmail.com>
2024-03-03 15:00:30 -0800

    UserID for all the pages

--- back-end/models/Users.js
+++ back-end/models/Users.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
     name: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    Image: {type: String, default: null},
 });
 
 //user is the new table name 
