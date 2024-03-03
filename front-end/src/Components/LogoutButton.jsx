204079286127e2beb32088a7d9426f049f6812df
Edmund <edmundyau1214@gmail.com>
2024-03-03 00:57:15 -0800

    cleaned up and added protected routes to pages

---
+++ front-end/src/Components/LogoutButton.jsx
@@ -0,0 +1,28 @@
+// LogoutButton.js
+import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const LogoutButton = () => {
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:3001/logout', {}, { withCredentials: true })
+        .then(result => {
+            console.log(result);
+            if (result.data.Logout) { 
+                navigate('/'); 
+            }
+        })
+        .catch(err => console.log(err));
+    };
+
+    return (
+        <button onClick={handleSubmit}>
+            LOG OUT
+        </button>
+    );
+};
+
+export default LogoutButton;
