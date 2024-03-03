73d91e58c458f17f646cffac2933855a9f41c53f
henrynvn09 <henrynvn09@gmail.com>
2024-03-02 21:51:20 -0800

    Update recipePreviewBox relatively
    add click handler and style of the text, image as relative

--- front-end/src/Components/RecipePreviewBox.jsx
+++ front-end/src/Components/RecipePreviewBox.jsx
@@ -1,12 +1,21 @@
 function RecipePreviewBox(props) {
+  const onClickHandler = () => {
+    // window.location.href = `/recipe/${props.id}`;
+    console.log("redirect to the props has id: ", props.id);
+  };
   return (
-    <div className="w-48 h-52 border border-b rounded-md m-3 shadow-md hover:shadow-lg bg-slate-50 transition-transform duration-300 transform hover:scale-105">
+    <div
+      className="border border-b rounded-md m-3 shadow-md hover:shadow-lg bg-slate-50 transition-transform duration-300 transform hover:scale-105"
+      onClick={onClickHandler}
+    >
       <img
         className="h-3/4 w-fill w-full object-cover "
         src={props.image}
         alt={props.title}
       />
-      <div className="text-xl text-center line-clamp-2">{props.title}</div>
+      <div className="text-center line-clamp-2 text-md md:text-lg">
+        {props.title}
+      </div>
     </div>
   );
 }
