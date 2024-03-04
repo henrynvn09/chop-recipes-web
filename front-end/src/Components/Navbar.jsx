54b397fec1830a8743292c9c0a94737d371ff1b0
henrynvn09 <henrynvn09@gmail.com>
2024-03-03 21:08:48 -0800

    Refactor Navbar

--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../Styles/Navbar.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../Styles/Navbar.css";
 
 const Navbar = () => {
   // State variable to hold the value of the input field
-  const [searchInput, setSearchInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
 
   // Event handler to update the search input value when Enter key is pressed
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleSearch();
     }
   };
 
   // Event handler to update the search input value when search button is clicked
   const handleSearch = () => {
-    console.log('Search Input:', searchInput);
-    setSearchInput('');
+    console.log("Search Input:", searchInput);
+    setSearchInput("");
     // Here you can perform any further actions with the searchInput value
   };
 
@@ -26,20 +26,20 @@ const Navbar = () => {
   };
 
   return (
-    <nav className='navbar'>
+    <nav className="navbar">
       <Link to="/home">
-        <img src="/chopLogoWhite.svg" alt="" className ='logo' />
+        <img src="/chopLogoWhite.svg" alt="" className="logo" />
       </Link>
-      <div className= "search-box">
-        <input 
-          type="text" 
-          placeholder='Search' 
-          value={searchInput} 
-          onChange={handleInputChange} 
-          onKeyPress={handleKeyPress} 
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchInput}
+          onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
         />
         <button className="search-button" onClick={handleSearch}>
-          <img src="/searchIcon.svg" alt="" className ='search-icon' />
+          <img src="/searchIcon.svg" alt="" className="search-icon" />
         </button>
       </div>
       <ul>
@@ -47,23 +47,30 @@ const Navbar = () => {
           <Link to="/home">Home</Link>
         </li>
         <li>
+          <Link to="/library">Library</Link>
+        </li>
+        <li>
           <Link to="/about">About</Link>
         </li>
         <li>
-          <Link to="/contacts">Contact</Link> 
+          <Link to="/contacts">Contact</Link>
         </li>
         <li>
           <Link to="/user">User</Link>
         </li>
         <li>
-          <Link to="/upload-recipe">Upload Recipe</Link>
+          <Link className="notoverflow" to="/upload-recipe">
+            Upload Recipe
+          </Link>
         </li>
         <li>
-          <Link to="/view-recipe">View Recipe</Link>
+          <Link className="notoverflow" to="/view-recipe">
+            View Recipe
+          </Link>
         </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
