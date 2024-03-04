2261843533078adce96b4e178a4b1186222ab8ca
Olivia-Emerson Ford <fordolivia0@gmail.com>
2024-03-04 14:34:53 -0800

    Added 'Poppins' and 'Roboto' fonts to the project and updated the styling of the 'Welcome' and 'RecipeOfTheDay' components on the HomePage using Tailwind CSS.

--- front-end/src/Pages/HomePage.jsx
+++ front-end/src/Pages/HomePage.jsx
@@ -4,26 +4,32 @@ import { Link } from 'react-router-dom'
 import '../Styles/global.css'
 import ProtectedRoute from "../Components/ProtectedRoute";
 import { useUser } from "../contexts/UserContent";
+
+import 'tailwindcss/tailwind.css';
+
+import 'tailwindcss/tailwind.css';
+
+import 'tailwindcss/tailwind.css';
+
 function Welcome() {
   return (
-    <div style={{ border: "1px solid black", padding: "20px", margin: "20px" }}>
-      <h1>Welcome to Chop!</h1>
-      <p>
-        Chop is an amazing platform where you can discover, share, and explore delicious recipes from around the world. Whether you're a professional chef or a home cook, Chop has something for everyone.
-      </p>
-    </div>
-  )
+      <div className="bg-red-200 rounded-full w-1/3 h-1/2 p-2 absolute left-0 top-20 box-border font-roboto flex flex-col items-center justify-center text-center">
+          <h1 className="font-bold italic text-lg mb-2">Welcome</h1>
+          <h1 className="font-bold italic text-lg mb-2">to</h1>
+          <h1 className="font-bold italic text-lg mb-2">Chop!</h1>
+          <p className="font-semibold italic text-xs">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut turpis velit. Integer nec facilisis velit. Fusce suscipit libero et odio facilisis, id fringilla nisi ultricies.
+          </p>
+      </div>
+  );
 }
 
 function RecipeOfTheDay() {
   return (
-    <div style={{ border: "1px solid black", padding: "20px", margin: "20px" }}>
-      <h1>Recipe of The Day</h1>
-      <p>
-        Chicken Soup!
-      </p>
-    </div>
-  )
+      <div className="bg-red-200 rounded-full w-1/3 h-auto p-2 absolute left-0 top-2/3 box-border font-roboto flex flex-col items-center justify-center text-center">
+          <h1 className="font-bold italic text-lg mb-2">Recipe Of The Day</h1>
+      </div>
+  );
 }
 
 function RandomRecipe() {
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif']
+      },
+      width: {
+        '1/3': '33.333333%',
+      },
+      height: {
+        '1/2': '50%',
+      }
+    },
   },
+  variants: {},
   plugins: [],
 };
