e4f610d146b15c803888d85905073fadb9f78cd7
henrynvn09 <henrynvn09@gmail.com>
2024-03-03 21:13:18 -0800

    refine Library Recipe page

--- front-end/src/Pages/LibraryRecipes.jsx
+++ front-end/src/Pages/LibraryRecipes.jsx
@@ -7,6 +7,8 @@ import React from "react";
 import RecipePreviewBox from "../Components/RecipePreviewBox";
 import ProtectedRoute from "../Components/ProtectedRoute";
 
+import Navbar from "../Components/Navbar";
+
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 function LibraryRecipes() {
@@ -128,36 +130,39 @@ function LibraryRecipes() {
     ));
 
   return (
-    <div className="flex flex-row">
-      <div className="h-screen sticky top-0 bg-gray-10">
-        {/* Left sidebar content */}
-        <div className="h-[50%] mb-12">
-          <h2 className="text-lg font-bold mb-2 p-5 pb-0">Filter by Tags</h2>
-          {/* Render tags dynamically */}
-          <div className="h-full overflow-y-scroll p-5 pt-0">{tags_html}</div>
-        </div>
-        <div className="h-[50%]">
-          <h2 className="text-lg font-bold mb-2  p-5 pb-0">
-            Filter by Ingredients
-          </h2>
-          {/* Render ingredients dynamically */}
-          <div className="h-full overflow-y-scroll  p-5 pt-0">
-            {ingredients_html}
+    <div>
+      <Navbar />
+      <div className="flex flex-row">
+        <div className="h-screen sticky mt-16 top-0 bg-gray-10">
+          {/* Left sidebar content */}
+          <div className="h-[32%] mb-12">
+            <h2 className="text-lg font-bold mb-2 p-5 pb-0">Filter by Tags</h2>
+            {/* Render tags dynamically */}
+            <div className="h-full overflow-y-scroll p-5 pt-0">{tags_html}</div>
+          </div>
+          <div className="h-[32%]">
+            <h2 className="text-lg font-bold mb-2  p-5 pb-0">
+              Filter by Ingredients
+            </h2>
+            {/* Render ingredients dynamically */}
+            <div className="h-full overflow-y-scroll  p-5 pt-0">
+              {ingredients_html}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex-4 flex flex-col w-[80%] py-5">
-        <h1 className="text-2xl font-bold flex-row pb-6">All Recipes</h1>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          placeholder="Search by title"
-          className="border border-gray-300 rounded px-2 py-1 mb-4 pr-24"
-        />
-        {/* Recipe preview boxes */}
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
-          {recipePreviewBoxes}
+        <div className="flex-4 flex flex-col w-[80%] py-5">
+          <h1 className="text-2xl font-bold flex-row pb-6">All Recipes</h1>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            placeholder="Search by title"
+            className="border border-gray-300 rounded px-2 py-1 mb-4 pr-24"
+          />
+          {/* Recipe preview boxes */}
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
+            {recipePreviewBoxes}
+          </div>
         </div>
       </div>
     </div>
