48f82821b1aa5663fb807aa19272d9907b6dcd84
Olivia-Emerson Ford <fordolivia0@gmail.com>
2024-03-04 22:27:54 -0800

    Added welcome banner for homepage and background

--- front-end/src/Pages/HomePage.jsx
+++ front-end/src/Pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import { Link } from 'react-router-dom'
-import '../Styles/global.css'
+
 import ProtectedRoute from "../Components/ProtectedRoute";
 import { useUser } from "../contexts/UserContent";
 
@@ -9,15 +9,23 @@ import 'tailwindcss/tailwind.css';
 
 function Welcome() {
   return (
-      <div className="bg-red-200 rounded-full w-1/3 h-1/2 p-2 absolute left-0 top-20 box-border font-roboto flex flex-col items-center justify-center text-center">
-          <h1 className="font-bold italic text-lg mb-2">Welcome to</h1>
-          <h1 className="font-bold italic text-lg mb-2">Chop!</h1>
-          <p className="font-semibold italic text-xs">
-          Whether you're a seasoned chef or a kitchen novice, prepare to embark on a flavor-packed adventure like no other. Dive into our recipe library, where a treasure trove of culinary delights awaits your discovery. With our intuitive search bar and ingredient-tagging magic, finding your next culinary masterpiece is as easy as pie. So, sharpen your knives and unleash your inner chef – let's Chop till we drop!
-          </p>
+    <div className="fixed top-24 left-4 w-1/3 h-40vh p-4 rounded-lg bg-custom-red">
+      <div className="text-center">
+        <p className="font-bold italic text-lg">Welcome to</p>
+        <p className="font-bold italic text-4xl">CHOP</p>
+        <p className="italic text-lg">
+          Whether you're a seasoned chef or a kitchen novice, prepare to embark
+          on a flavor-packed adventure like no other. Dive into our recipe
+          library, where a treasure trove of culinary delights awaits your
+          discovery. With our intuitive search bar and ingredient-tagging
+          magic, finding your next culinary masterpiece is as easy as pie. So,
+          sharpen your knives and unleash your inner chef – let's Chop till we
+          drop!
+        </p>
       </div>
+    </div>
   );
-}
+};
 
 function RecipeOfTheDay() {
   return (
@@ -81,7 +89,7 @@ function HomePage(){
   const {userID} = useUser(); 
   ProtectedRoute();
   return (
-      <div>
+    <div style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/stripes-bg.jpg'})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', height: '100vh', overflow: 'auto' }}>
           <Navbar />
           <div>
               <Welcome></Welcome>
