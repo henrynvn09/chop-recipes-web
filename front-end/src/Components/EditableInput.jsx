b0bdfacbc862d30c4ee1fad1c270ef488de6b44e
Olivia-Emerson Ford <fordolivia0@gmail.com>
2024-03-07 15:15:23 -0800

    Installed npm sass and expanded the EditableInput a little

--- front-end/src/Components/EditableInput.jsx
+++ front-end/src/Components/EditableInput.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-//import css from '../Styles/EditableInput.module.scss'
+import css from '../Styles/EditableInput.module.scss'
 import PropTypes from 'prop-types'
 import 'tailwindcss/tailwind.css'
-import { mdiAccountBadge } from '@mdi/js'
+import { mdiPencil } from '@mdi/js'
 import ButtonIcon from './ButtonIcon'
 
 function EditableInput(props) {
@@ -20,7 +20,12 @@ function EditableInput(props) {
                     onClick={turnOnEditMode}
                 />
             </span>
-            <ButtonIcon path={mdiAccountBadge} />
+            <ButtonIcon 
+                className='hover: bg-gray-200 rounded-full'
+                path={mdiPencil}
+                size={0.65}
+                color='grey'
+            />
             
             <button>
                 {/* edit button*/}
