d6c794212bc729786b4c6084b77ee5adb6322866
Edmund <edmundyau1214@gmail.com>
2024-03-07 18:32:40 -0800

    fixed responsivity of userpage, homepage, and library

--- front-end/src/Pages/HomePage.jsx
+++ front-end/src/Pages/HomePage.jsx
@@ -130,73 +130,69 @@ function HomePage(){
             <img src='/allfoodimg.jpg' alt="All Food" className="w-full h-full object-cover" />
             <div className="absolute inset-0 bg-black transition duration-300 ease-in-out bg-opacity-50 hover:bg-opacity-75 ">
 
-              <a href="/library" className="absolute inset-0 flex justify-center mt-40 z-0">
+              <a href="/library" className="absolute inset-0 flex justify-center heading1 z-0">
                   <h1 className="text-white text-2xl md:text-4xl lg:text-7xl font-bold">Never Takeout Again</h1>
               </a>
-              <a href="/library" className="absolute inset-0 flex justify-center mt-80 z-0">
+              <a href="/library" className="absolute inset-0 flex justify-center heading2 z-0">
                   <h3 className="text-custom-grey text-sm md:text-md lg:text-lg italic">At Chop, we offer quality services for all!</h3>
               </a>
             </div>
         </div>
 
           
-          <div className="centered-grid absolute flex justify-center z-1000">
-                  <div className="grid grid-cols-3 gap-[4rem] max-w-5xl "
+          <div className=" centered-grid absolute flex justify-center items-center z-1000">
+                  <div className=" grid grid-cols-3 place-items-center lg:gap-y-[1.5rem] lg:gap-x-[3rem] max-w-xl 2xl:gap-y-[1.5rem] 2xl:gap-x-[15rem] 4xl:gap-y-[1.5rem] 4xl:gap-x-[15rem]"
                   >
             
-                    <div className="bg-custom-darkgreen rounded-[4rem] transition duration-500 ease-in-out hover:scale-110 hover:bg-custom-green p-10">
-                        <button onClick={() => handleLibraryClick()} className="  rounded-lg h-64 box-border font-roboto text-lg font-bold italic ">
-                          <div className="justify-center flex-col ">
+                    <div className="gridbox bg-custom-darkgreen lg:rounded-[2rem] 2xl:rounded-[3rem] transition duration-500 ease-in-out hover:scale-110 hover:bg-custom-green ">
+                        <button onClick={() => handleLibraryClick()} className="  rounded-lg box-border font-roboto text-lg font-bold italic flex flex-col justify-content items-center">
+                          <div className="insidebox justify-center flex flex-col items-center">
+                            <img className = "imagebox" src='/cookbook.png' alt="Cookbook"  />
+                            <div className=" bg-opacity-50">Browse Library</div>
+                          </div>
+                        </button>
+                    </div>
 
-                            <img src='/cookbook.png' alt="Cookbook"  />
-                            <div className=" bg-opacity-50">Browse our Library</div>
+                    <div className="gridbox bg-custom-darkgreen rounded-[2rem] 2xl:rounded-[3rem] transition duration-500 ease-in-out hover:scale-110 hover:bg-custom-green ">
+                        <button onClick={() => handleUploadRecipeClick()} className=" rounded-lg box-border font-roboto text-lg font-bold italic flex  justify-content items-center">
+                            <div className="insidebox justify-center flex flex-col items-center">
+                              <img className = "imagebox" src={'/whisk-square.png'} alt="Whisk"  />
+                              <div className=" bg-opacity-50">Upload Recipe</div>
                             </div>
                         </button>
                     </div>
 
-                    <div className="bg-custom-darkgreen rounded-[4rem]  transition duration-500 ease-in-out hover:scale-110 hover:bg-custom-green p-10">
-                        <button onClick={() => handleUploadRecipeClick()} className=" rounded-lg h-64 box-border font-roboto text-lg font-bold italic ">
-                                                <div className="justify-center flex-col">
-
-                            <img src={'/whisk-square.png'} alt="Whisk"  />
-                            <div className=" bg-opacity-50">Upload New Recipe</div>
+                    <div className="gridbox bg-custom-darkgreen rounded-[2rem] 2xl:rounded-[3rem] transition duration-500 ease-in-out hover:scale-110 hover:bg-custom-green">
+                        <button onClick={() => handleContactClick()} className="rounded-lg box-border font-roboto text-lg font-bold italic flex justify-content items-center">
+                            <div className="insidebox  justify-center flex flex-col items-center">
+                              <img className = "imagebox" src={'/mail-square.png'} alt="Contact"  />
+                              <div className=" bg-opacity-50">Contact Us</div>
                             </div>
                         </button>
                     </div>
 
-                    <div className="bg-custom-darkgreen rounded-[4rem]   transition duration-500 ease-in-out hover:scale-110 hover:bg-custom-green p-10">
-                        <button onClick={() => handleContactClick()} className="rounded-lg h-64 box-border font-roboto text-lg font-bold italic ">
-                                                <div className="justify-center flex-col">
-
-                            <img src={'/mail-square.png'} alt="Contact"  />
-                            <div className=" bg-opacity-50">Contact Us</div>
+                    <div className="gridbox bg-custom-darkgreen rounded-[2rem] 2xl:rounded-[3rem] transition duration-500 ease-in-out hover:scale-110 hover:bg-custom-green">
+                        <button onClick={() => handleProfileClick(userID)} className="rounded-lg  box-border font-roboto text-lg font-bold italic flex  justify-content items-center">
+                            <div className="insidebox justify-center flex flex-col items-center">
+                              <img className = "imagebox" src={'/chefhat.png'} alt="Chef Hat"  />
+                              <div className="bg-opacity-50">Chef's Profile</div>
                             </div>
                         </button>
                     </div>
 
-                    <div className="bg-custom-darkgreen rounded-[4rem]   transition duration-500 ease-in-out hover:scale-110 hover:bg-custom-green p-10">
-                        <button onClick={() => handleProfileClick(userID)} className="rounded-lg h-64 box-border font-roboto text-lg font-bold italic ">
-                            <div className="justify-center flex-col">
-                            <img src={'/chefhat.png'} alt="Chef Hat"  />
-                            <div className="bg-opacity-50">Chef's Profile</div>
+                    <div className="gridbox bg-custom-darkgreen rounded-[2rem] 2xl:rounded-[3rem]  transition duration-500 ease-in-out hover:scale-110 hover:bg-custom-green">
+                        <button onClick={() => handleRandomRecipeClick()} className="rounded-lg box-border font-roboto text-lg font-bold italic flex  justify-content items-center ">
+                            <div className="insidebox justify-center flex flex-col items-center">
+                              <img className = "imagebox" src={'/blender-newest.png'} alt="Blender" />
+                              <div className=" bg-opacity-50">Random Recipe</div>
                             </div>
                         </button>
                     </div>
-
-                    <div className="bg-custom-darkgreen rounded-[4rem]   transition duration-500 ease-in-out hover:scale-110 hover:bg-custom-green p-10">
-                        <button onClick={() => handleRandomRecipeClick()} className="rounded-lg h-64 box-border font-roboto text-lg font-bold italic ">
-                            <div className="justify-center flex-col">
-
-                            <img src={'/blender-newest.png'} alt="Blender" />
-                            <div className=" bg-opacity-50">Random Recipe</div>
-                            </div>
-                        </button>
-                    </div>
-                    <div className="bg-custom-darkgreen rounded-[4rem]  transition duration-500 ease-in-out hover:scale-110 hover:bg-custom-green p-10">
-                        <button onClick={handleShare} className=" rounded-lg w-60 h-60 box-border font-roboto text-lg font-bold italic  flex flex-col">
-                            <div className="justify-center flex-col">
-                              <img src={'/chef.png'} alt="Share" />
-                              <div className="bg-opacity-50">Share with a Friend</div>
+                    <div className="gridbox bg-custom-darkgreen rounded-[2rem] 2xl:rounded-[3rem] transition duration-500 ease-in-out hover:scale-110 hover:bg-custom-green">
+                        <button onClick={handleShare} className=" rounded-lg  box-border font-roboto text-lg font-bold italic flex justify-content items-center">
+                            <div className="insidebox justify-center flex flex-col items-center">
+                              <img className = "imagebox" src={'/chef.png'} alt="Share" />
+                              <div className="bg-opacity-50">Share with Friend</div>
                             </div>
                         </button>
                     </div>
