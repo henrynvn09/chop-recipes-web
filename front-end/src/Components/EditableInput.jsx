d7c14a5a7dd550a7a740888b614c5349aa3baa05
Olivia-Emerson Ford <fordolivia0@gmail.com>
2024-03-09 12:18:47 -0800

    Adding more styling to the textinput box for user description. Makes the box 96w and 32h (max 72h) and scrollable. Also adds styling to the scrollable bar in the textbox

--- front-end/src/Components/EditableInput.jsx
+++ front-end/src/Components/EditableInput.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import 'tailwindcss/tailwind.css'
 import { mdiPencil } from '@mdi/js'
 import ButtonIcon from './ButtonIcon'
+import '../Styles/Scrollbar.css'
 
 function EditableInput({value, type = '', ...props}) {
     const [isEditMode, setIsEditMode] = React.useState(false)
@@ -12,24 +13,31 @@ function EditableInput({value, type = '', ...props}) {
         setIsEditMode(true);
         inputRef.current.focus();
     }
+    const adjustHeight = (element) => {
+        element.style.height = "auto";
+        element.style.height = element.scrollHeight + "px";
+    }
+    const handleChange = (event) => {
+        adjustHeight(event.target);
+    }
     return (
         <div className='flex justify-center items-center'>
           <span className="relative"> 
-            <input 
+            <textarea
               ref={inputRef}
-              type={type} 
               autoFocus
               value={props.value} 
               readOnly={!isEditMode} 
               onClick={turnOnEditMode}
               onBlur={() => setIsEditMode(false)}
-              className="w-1/2 py-2 px-4 font-roboto text-lg border-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 hover:bg-blue-100 transition-colors"
+              onChange={handleChange}
+              className="w-96 max-w-96 h-auto min-h-32 max-h-72 py-2 px-4 font-roboto text-sm border-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 hover:bg-blue-100 transition-colors resize-none overflow-auto custom-scrollbar"
               {...props}
             />
             {!isEditMode && (
               <ButtonIcon 
                 onClick={turnOnEditMode}
-                className='hover:bg-gray-200 rounded-full absolute right-8 top-1/2 transform -translate-y-1/2'
+                className='hover:bg-gray-200 rounded-full absolute bottom-6 right-2.5'
                 path={mdiPencil}
                 size={0.65}
                 color='grey'
