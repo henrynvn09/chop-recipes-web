47f2dd50303ac5347f9fc76d9bb455015bd0e786
Olivia-Emerson Ford <fordolivia0@gmail.com>
2024-03-09 12:23:29 -0800

    Minor change: limits the textbox to only allow 2k characters

--- front-end/src/Components/EditableInput.jsx
+++ front-end/src/Components/EditableInput.jsx
@@ -32,6 +32,7 @@ function EditableInput({value, type = '', ...props}) {
               onBlur={() => setIsEditMode(false)}
               onChange={handleChange}
               className="w-96 max-w-96 h-auto min-h-32 max-h-72 py-2 px-4 font-roboto text-sm border-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 hover:bg-blue-100 transition-colors resize-none overflow-auto custom-scrollbar"
+              maxLength={2000}
               {...props}
             />
             {!isEditMode && (
